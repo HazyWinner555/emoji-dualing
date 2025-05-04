f3bc37efb93d5d8de9ef70ed003eed153860dbdb
HazyWinner55 <yazirburnside@gmail.com>
2025-05-04 15:11:02 -0400

    connecting to server (2)
    cleaning up, refer to "connecting to server (1)"

--- emoji-dueling/src/Pages/Home.jsx
+++ emoji-dueling/src/Pages/Home.jsx
@@ -7,42 +7,6 @@
     The only non-Page Component that needs updating is NickNameInput.
 */
 
-<<<<<<< Updated upstream
-import { useNavigate } from "react-router-dom"
-import logo from "../assets/Emoji Dueling Logo.png"
-import { useEffect, useState } from "react"
-import "../css/Home.css"
-import NickNameInput from "../components/NicknameInput"
-import JoinRoom from "../components/JoinRoom"
-import { useServer } from '../components/ServerContext';
-import RoomCode from "../components/RoomCode"
-function Home() {
-    // const { send, connected, messages, roomState, requestRoomCreation } = useServer();
-
-
-    const navigate = useNavigate()
-    // const [hostRoomLink, setHostRoomLink] = useState("")
-    return (
-        <>
-            <div className="container">
-
-                <img src={logo} className="logo" />
-                <NickNameInput />
-                <JoinRoom />
-                <button className="hostRoomButton" onClick={() => {
-                    // requestRoomCreation((roomID) => {                        when server integration works properly, this will be how.
-                    //     console.log("Room created with ID:", roomID);
-                    //     navigate(`/${roomID}/host/lobby`);
-                    // });
-                    navigate(`${Math.random().toString(36).slice(2, 7)}/host/lobby`)
-                }}>
-                    Host Room
-                </button>
-
-            </div>
-        </>
-    )
-=======
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/Emoji Dueling Logo.png";
 import { useEffect, useState, useCallback } from "react";
@@ -93,7 +57,6 @@ function Home() {
             <button onClick={createRoomAndNavigate}>Host Room</button>
         </div>
     );
->>>>>>> Stashed changes
 }
 
 export default Home;
